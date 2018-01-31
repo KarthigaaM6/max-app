0ed8bfd01a984275aa11530ecb3a302cfe8677f8
adapt <keerthana.n10@wipro.com>
2018-01-31 17:06:11 +0530

    testcase passed

--- tests/sample_test.js
+++ tests/sample_test.js
@@ -1,19 +1,24 @@
 module.exports = {
-  'Demo test Google' : function (client) {
+	before: function (browser) {
+		console.log("Setting up...");
+		console.log('Process Version : ' + process.version);
+		browser
+			.url(browser.launchUrl)
+	},
 
-    console.log('title: ', client.url('http://www.google.com').title);
-    client
-      .url('http://www.google.com')
-      //.waitForElementVisible('body', 1000)
-      .assert.title('Google')
-      // .assert.visible('input[type=text]')
-      .end();
-      // .setValue('input[type=text]', 'rembrandt van rijn')
-      // .waitForElementVisible('button[name=btnG]', 1000)
-      // .click('button[name=btnG]')
-      // .pause(1000)
-      // .assert.containsText('ol#rso li:first-child',
-      //   'Rembrandt - Wikipedia')
-      // .end();
-  }
-};
+	after: function (browser, done) {
+		browser.end(() => {
+			console.log("Closing down..."); done();
+		})
+	},
+
+	beforeEach: function (browser) {
+		browser.pause(2000).useCss()
+	},
+
+	"Test 1 : Check If Max Health Care URL is accessibles ": function (browser) {
+		browser.waitForElementVisible('body', 1000)
+			.assert.title('Max Health Care')
+	},
+	
+}
